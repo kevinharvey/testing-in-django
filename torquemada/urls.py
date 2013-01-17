f6d5c58646e01d95422fc9f395b63070b6a75c8b
kcharvey <kcharvey@gmail.com>
2013-01-17 15:45:11 -0500

    forget it. using an existing client as a CDN

--- torquemada/urls.py
+++ torquemada/urls.py
@@ -19,6 +19,3 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
-
-# this is not very efficient :)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
