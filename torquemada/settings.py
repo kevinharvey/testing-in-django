86d1f8c1c89d163e3ceacb71a742b88fe736ce37
kcharvey <kcharvey@gmail.com>
2013-01-17 15:29:12 -0500

    clean up error pages. toss top-bar

--- torquemada/settings.py
+++ torquemada/settings.py
@@ -1,7 +1,8 @@
 # Django settings for torquemada project.
 import os
+OS_PATH = os.path.dirname(__file__)
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -57,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(OS_PATH, 'static/'.replace('\\', '/'))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -164,3 +165,8 @@ if os.environ.get('DATABASE_URL'):
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
     DATABASES['default'] =  dj_database_url.config()
+
+try:
+	from local_settings import *
+except:
+	pass
--- torquemada/urls.py
+++ torquemada/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +19,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
