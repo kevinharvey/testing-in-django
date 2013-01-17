c331dcfa6217b94ba0288d2b4fd763913111ebdc
kcharvey <kcharvey@gmail.com>
2013-01-17 12:12:37 -0500

    extend func test to try to vote

--- questions/tests/func_tests.py
+++ questions/tests/func_tests.py
@@ -29,8 +29,14 @@ class QuestionsTest(LiveServerTestCase):
 		question_2 = self.browser.find_element_by_css_selector("div#trq-question-2 h4")
 		self.assertEqual(question_2.text, "Does Selenium only work in Firefox?")
 		
-		# She sees a question that she'd like to have answered,
-		# and votes it up
+		# She sees a question that she'd like to have answered, and votes it up
+		vote_for_2 = self.browser.find_element_by_css_selector("div#trq-question-2").click().follow()
+		vote_tally = self.browser.find_element_by_css_selector("div#trq-question-2 .trq-vote-count")
+		self.assertEqual(vote_tally.text, "Votes: 1")
+		
+		# Her voted up question is now at the top of the list
+		all_questions = self.browser.find_elements_by_css_selector("div.trq-question")
+		self.assertEqual(all_questions[0], self.browser.find_element_by_css_selector("div#trq-question-2"))
 		
 		# She sees a silly question and votes it down. No
 		# time for that type of stuff in a Django talk
